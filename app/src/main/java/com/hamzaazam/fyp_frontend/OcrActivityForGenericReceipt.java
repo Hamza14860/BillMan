02d48b98a609534787611a461d55c13d514879f0
Hamza A <42303641+Hamza14860@users.noreply.github.com>
2020-06-04 14:07:22 +0500

    Removed crashing bug of Bill Image Fragment

--- app/src/main/java/com/hamzaazam/fyp_frontend/OcrActivityForGenericReceipt.java
+++ app/src/main/java/com/hamzaazam/fyp_frontend/OcrActivityForGenericReceipt.java
@@ -3,12 +3,12 @@ package com.hamzaazam.fyp_frontend;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OcrActivityForGenericReceipt extends OcrActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-    }
-}
+//public class OcrActivityForGenericReceipt extends OcrActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//
+//    }
+//}
